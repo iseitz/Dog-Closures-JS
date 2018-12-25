e338313efa183b414e041e2a7da5c5723179cb7b
Irina Karchebnaya <irenika@gmail.com>
2018-12-25 12:31:42 +0600

    first commit fully working

---
+++ dogs.js
@@ -0,0 +1,28 @@
+function dogTalk() {
+  let commonDogName = 'Rover';
+  let unCommonDogName = 'Susan';
+
+  let stateDogNameOpinion = (function() {
+    return `I believe ${commonDogName} is an excellent dog name. The name ${unCommonDogName} is a bad dog name.`;
+  });
+
+  let stateDogNameFact = (function() {
+    return `I have never met a dog called ${unCommonDogName} while several dogs are called ${commonDogName}.`;
+  });
+
+  let callDogs = (function() {
+    return `Hey, come here ${commonDogName} and ${unCommonDogName}!`;
+  });
+
+  return {
+    stateDogNameFact,
+    stateDogNameOpinion,
+    callDogs
+  };
+
+}();
+
+let dog = dogTalk();
+dog.stateDogNameFact();
+dog.stateDogNameOpinion();
+dog.callDogs();
